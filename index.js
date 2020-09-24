5eb2a7ded6352d72a77963c34c7d4a96798fc461
judehunter <matisowagm@gmail.com>
2020-09-24 13:42:18 +0200

    Variables

--- index.js
+++ index.js
@@ -134,5 +134,17 @@ module.exports = {
     'yoda': 2,
     'strict': 0,
 
+    //--- Variables
+    'init-declarations': 0,
+    'no-delete-var': 2,
+    'no-label-var': 2,
+    'no-restricted-globals': 0,
+    'no-shadow': 0,
+    'no-shadow-restricted-names': 2,
+    'no-undef': 2,
+    'no-undef-init': 0,
+    'no-undefined': 0,
+    //# 'no-unused-vars': [1, {ignoreRestSiblings: true, varsIgnorePattern: '^_'}],
+    'no-use-before-define': 2,
   }
 }
