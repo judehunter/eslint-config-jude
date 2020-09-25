468d4495ee624b0ffbd2d38adbe3a9b462016b8b
judehunter <matisowagm@gmail.com>
2020-09-25 18:33:19 +0200

    Add parserOptions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  parserOptions: {sourceType: 'module'},
   rules: {
     //--- Possible Errors
     'for-direction': 1,
